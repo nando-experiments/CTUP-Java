981b68667131ae5306831c76c35e16905990aa88
Fernando Moreira <nandomoreira.me@gmail.com>
2015-10-23 21:17:31 -0200

    Mostra vetor, maior elemento

---
+++ Ex.23-10-2015/src/br/com/ctup/dsj/Principal.java
@@ -0,0 +1,26 @@
+package br.com.ctup.dsj;
+
+import java.util.Vector;
+
+public class Principal
+{
+	public static void main(String[] args) {
+		Vetor v1 = new Vetor();
+		Vector<Integer> vet = new Vector<Integer>(2, 3);
+		
+		vet.addElement(1);
+		vet.addElement(23);
+		vet.addElement(5);
+		vet.addElement(4);
+		vet.addElement(6);
+		vet.addElement(30);
+		vet.addElement(7);
+		vet.addElement(10);
+		v1.MostraVetor(vet);
+
+		System.out.println("");
+		System.out.println("---");
+		
+		v1.MaiorElemento(vet);
+	}
+}
---
+++ Ex.23-10-2015/src/br/com/ctup/dsj/Vetor.java
@@ -0,0 +1,35 @@
+package br.com.ctup.dsj;
+
+import java.util.Vector;
+
+public class Vetor
+{
+	public void MostraVetor(Vector<Integer> vet) {
+		int count = 0;
+		
+		for (int i = 0; i < vet.size(); i++) {
+		    System.out.print(vet.get(i));
+		    
+		    count = i + 1;
+		    if(count < vet.size()) {
+		    	System.out.print(", ");
+		    }
+		    //System.out.println(count);
+		}
+	}
+	
+	public void MaiorElemento(Vector<Integer> vet) {
+		int maior = 0;
+		int num = 0;
+		
+		for (int i = 0; i < vet.size(); i++) {
+			num = vet.get(i);
+			
+			if(num > maior) {
+				maior = num;
+			}
+		}
+		
+		System.out.println("O maior número do vetor é: " + maior);
+	}
+}
